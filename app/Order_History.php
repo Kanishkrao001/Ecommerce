f55eaf73d68d2dff59014a087a52923291f69249
Kanishk Yadav <kanishkrao001@gmail.com>
2021-08-02 17:47:20 +0530

    created a jwt creater functin prototype

--- app/Order_History.php
+++ app/Order_History.php
@@ -8,4 +8,23 @@ class Order_History extends Model
 {
     //
     protected $table="order_histories";
+
+    public static function GetOrderDetails($id)
+    {
+        $all = Order_History::join('products', 'products.product_id' , '=', 'order_histories.product_id')->where('order_id',$id)->get();
+        return $all;
+    }
+}
+
+
+class generateJWT()
+{
+
+    header = { "algorithm": "abc","type" : "JWT" };
+    payload = { "issuer":"abc.com", "subject":"random token", "expire": 10000000,"info":"random info"};
+
+    $signature = hash(header) + "." + hash(payload);
+    $signature = hash($signature);
+
+    $token = hash(header) + "." + hash(payload) + "." $signature;
 }
