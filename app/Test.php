7a084b91a6e455cb09a0260e15ed562a876abc1c
Kanishk Yadav <kanishkrao001@gmail.com>
2021-08-05 16:21:36 +0530

    Paginate without paginate function

--- app/Test.php
+++ app/Test.php
@@ -3,10 +3,52 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
+use App\Product;
+use App\User;
+use App\Order;
+use App\Order_History;
 
 class Test extends Model
 {
     //
-    protected $connection = 'mysql2';
-    protected $table = 'users';
+    // protected $connection = 'mysql1'
+    protected $table = "orders";
+    public static function getorder($order_id)
+    {
+        $orders = Test::where('id', $order_id)->get();
+        return $orders;
+    }
+
+    public static function GetOrderByDate($date)
+    {
+        $orders = Test::where('customer_id', 3)->where('created_at', '>', $date)->get();
+        return $orders;
+    }
+
+    public static function GetOrderByModeOfPayment($mode_of_payment)
+    {
+        $orders = Test::where('mode_of_payment', $mode_of_payment)->get();
+        return $orders;
+    }
+
+    public static function getOrdersFromAllFilters($order_no, $date, $mode_of_payment, $limit, $page_no)
+    {
+        // return $order_no;
+        $users = Test::select('*');
+
+        if(!empty($order_no))
+        {
+            $users = $users->where('id', $order_no);
+        }
+        if(!empty($date))
+        {
+            $users = $users->where('customer_id', 3)->where('created_at', '>=', $date);
+        }
+        if(!empty($mode_of_payment))
+        {
+            $users = $users->where('mode_of_payment', $mode_of_payment);
+        }
+        return $users->skip($page_no*$limit)->take($limit)->get();
+    }
 }
